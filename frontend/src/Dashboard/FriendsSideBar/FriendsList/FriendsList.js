5b58f052c70d85ca6dacba07b0d3eba78856a6d1
Abhishek Agarwal <abhishek4075@gmail.com>
2023-12-30 22:27:26 +0530

    Added: Friends List UI

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { styled } from "@mui/system";
+import FriendsListItem from "./FriendsListItem";
+
+const DUMMY_FRIENDS = [
+
+]
 
 const MainContainer = styled("div")({
 	flexGrow: 1,
@@ -7,9 +12,17 @@ const MainContainer = styled("div")({
 });
 
 const FriendsList = ({ friends, onlineUsers }) => {
+	console.log('DUMMY_FRIENDS', DUMMY_FRIENDS)
 	return (
 		<MainContainer>
-
+			{DUMMY_FRIENDS.map((f) => (
+				<FriendsListItem
+					username={f.username}
+					id={f.id}
+					key={f.id}
+					isOnline={f.isOnline}
+				/>
+			))}
 		</MainContainer>
 	);
 };
