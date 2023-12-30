f39538e15785de24799c810b5f5addd59cb1932e
Abhishek Agarwal <abhishek4075@gmail.com>
2023-12-30 19:48:08 +0530

    Added: Authentication System

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const PORT = process.env.PORT || process.env.API_PORT;
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || process.env.API_PORT;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+const server = http.createServer(app);
 
 mongoose
     .connect(process.env.MONGO_URI)
