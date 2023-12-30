9dd73859c280d2cff5401e0ce703cabdf9522d47
Abhishek Agarwal <abhishek4075@gmail.com>
2023-12-30 20:31:29 +0530

    Added: Login Page

---
+++ frontend/src/authPages/LoginPage/LoginPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import AuthBox from '../../shared/components/AuthBox'
+import LoginPageHeader from './LoginPageHeader'
+import LoginPageInputs from './LoginPageInputs'
+import LoginPageFooter from './LoginPageFooter'
+import { validateLoginForm } from '../../shared/utils/validators'
+
+const LoginPage = () => {
+
+	const [mail, setMail] = useState("");
+	const [password, setPassword] = useState("");
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	useEffect(() => {
+		setIsFormValid(validateLoginForm({ mail, password }));
+	}, [mail, password, setIsFormValid]);
+
+	const handleLogin = () => {
+
+	};
+
+	return (
+		<AuthBox>
+			<LoginPageHeader />
+			<LoginPageInputs
+				mail={mail}
+				setMail={setMail}
+				password={password}
+				setPassword={setPassword}
+			/>
+			<LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin} />
+		</AuthBox>
+	)
+}
+
+export default LoginPage
